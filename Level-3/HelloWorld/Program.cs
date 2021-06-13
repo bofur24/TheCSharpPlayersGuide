0c3e906b90010b7dd788d15b0f8f0496e66aea6a
keithheffington <keith.heffington@gmail.com>
2021-06-13 06:56:03 -0500

    Multi console writelines

--- Level-3/HelloWorld/Program.cs
+++ Level-3/HelloWorld/Program.cs
@@ -7,6 +7,10 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Keith.");
+            Console.WriteLine("I am new to this land");
+            Console.WriteLine("Can you help me understand why I am here?");
+            Console.WriteLine("This land is new.");
         }
     }
 }
