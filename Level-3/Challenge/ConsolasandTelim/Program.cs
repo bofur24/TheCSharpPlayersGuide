0b4b8e7288b055269c8d67f7d742a41662db8a0f
keithheffington <keith.heffington@gmail.com>
2021-06-13 07:02:50 -0500

    Challenge 1 Complete 50XP

---
+++ Level-3/Challenge/ConsolasandTelim/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsolasandTelim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bread is ready.");
+            Console.WriteLine("Whos is the bread for?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Note: " + name + " got bread.");
+        }
+    }
+}
