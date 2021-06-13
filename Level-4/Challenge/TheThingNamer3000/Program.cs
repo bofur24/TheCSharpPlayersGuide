c56632512b41686d534fbd3caddc5d95e09991c6
keithheffington <keith.heffington@gmail.com>
2021-06-13 07:32:17 -0500

    Challenge 2 Complete

---
+++ Level-4/Challenge/TheThingNamer3000/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheThingNamer3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of thing are we talking about?");
+
+            // Type of thing
+            string a = Console.ReadLine();
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+            // Description
+            string b = Console.ReadLine();
+
+            // of doom text
+            string c = "doom"; // removed of to fix of of bug
+
+            /* the number */
+            string d = "3000";
+
+            Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
+        }
+    }
+}
