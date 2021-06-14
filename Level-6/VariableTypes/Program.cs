48e37c5efc9cf0a730f9a38b2011c32c37d6486c
Keith Heffington <lightingcount_@hotmail.com>
2021-06-14 14:44:46 -0500

    Started Level 6

---
+++ Level-6/VariableTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte aSingleByte = 34;
+            aSingleByte = 17;
+
+            short aNumber = 5039;
+            aNumber = -4354;
+
+            long aVeryBigNumber = 395904282569;
+            aVeryBigNumber = 13;
+
+            Console.WriteLine("Byte = " + aSingleByte);
+            Console.WriteLine("Short = " + aNumber);
+            Console.WriteLine("Long = " + aVeryBigNumber);
+
+        }
+    }
+}
