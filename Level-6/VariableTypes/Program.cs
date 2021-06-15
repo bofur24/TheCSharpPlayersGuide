f19d15d72f21d55ff3f09df6bd62f9683ce01ca5
Keith Heffington <lightingcount_@hotmail.com>
2021-06-15 09:16:33 -0500

    Working on different types

--- Level-6/VariableTypes/Program.cs
+++ Level-6/VariableTypes/Program.cs
@@ -15,10 +15,24 @@ namespace VariableTypes
             long aVeryBigNumber = 395904282569;
             aVeryBigNumber = 13;
 
+            ulong aVeryBigNumber1 = 10000000000u;
+            int thirteen = 0b00001101;
+            int theColorMagenta = 0xff00ff;
+            char aLetter = '\u0061'; // 'a'
+            double number1 = 3.5623;
+            float number2 = 3.5623f;
+            decimal number3 = 3.5623m;
+
             Console.WriteLine("Byte = " + aSingleByte);
             Console.WriteLine("Short = " + aNumber);
             Console.WriteLine("Long = " + aVeryBigNumber);
-
+            Console.WriteLine("Ulong = " + aVeryBigNumber1);
+            Console.WriteLine("Binary literal = " + thirteen);
+            Console.WriteLine("Color Magenta = " + theColorMagenta);
+            Console.WriteLine("Char letter = " + aLetter);
+            Console.WriteLine("Double = " + number1);
+            Console.WriteLine("Float = " + number2);
+            Console.WriteLine("Decimal = " + number3);
         }
     }
 }
