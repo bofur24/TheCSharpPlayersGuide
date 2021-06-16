6cb53c1f5d3b70e73836db8e86bece14a2d33f62
Keith Heffington <lightingcount_@hotmail.com>
2021-06-16 14:43:47 -0500

    Started level 7

---
+++ Level-7/operations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1 + 2;
+            Console.WriteLine(a);
+
+            int b = 5 - 2;
+            Console.WriteLine(b);
+
+            int c;                      // Declaring the variable c;
+            c = 9 - 2;                  // Assigning a value to c, using some math;
+            c = 3 + 3;                  // Another assignment.
+
+            Console.WriteLine(c);
+
+            int d = 3 + 1;               // Declaring d and assigning a value to d all at once
+            d = 1 + 2;                   // Assigning a second value to b.
+
+            Console.WriteLine(d);
+
+            int e = 1;
+            int f = e + 4;
+            int g = e - f;
+
+            Console.WriteLine("e = " + e + " f = " + f + " g = " + g);
+
+            float totalPies = 4;
+            float slicesPerPie = 8;
+            float totalSlices = totalPies * slicesPerPie;
+
+            Console.WriteLine("Total Slices of pie " + totalSlices);
+
+            double moneyMadeFromGame = 100000;
+            double totalProgrammers = 4;
+            double moneyPerPerson = moneyMadeFromGame / totalProgrammers; // We're rich!
+
+            Console.WriteLine("Money per person is " + moneyPerPerson);
+
+
+
+
+        }
+    }
+}
