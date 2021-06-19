63d8ce27e765194ecfe3c9e826857b7b7ab0f2cd
keithheffington <keith.heffington@gmail.com>
2021-06-19 02:27:56 -0500

    Completed the challenge

---
+++ Challenges/TheFourSistersandTheDuckbear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TheFourSistersandTheDuckbear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Objectives:
+                Create a program that lets the user enter the number of chocolate eggs gathered that day
+                Using / and % compute how many eggs each sister should get and how many are left over fo the duckbear.
+             */
+
+            Console.WriteLine("How many chocolate eggs were gathered today?");
+            string chocEggsText;
+            chocEggsText = Console.ReadLine();
+            int chocEggs = Convert.ToInt32(chocEggsText);
+
+            int result = chocEggs / 4;
+            float remainder = chocEggs % 4;
+
+            Console.WriteLine("Total eggs each sister gets " + result);
+            Console.WriteLine("Total eggs the Duckbear gets " + remainder);
+        }
+    }
+}
