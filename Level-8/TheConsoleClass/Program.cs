df07cc12082d6b4bebac69be2def62191db37f11
Keith Heffington <lightingcount_@hotmail.com>
2021-06-22 14:51:50 -0500

    Started Level-8

---
+++ Level-8/TheConsoleClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheConsoleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            Console.WriteLine("Press any key when you're ready to begin.");
+            Console.ReadKey(true); // overloaded 
+
+            Console.Write("What is your name human? ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + userName);
+        }
+    }
+}
