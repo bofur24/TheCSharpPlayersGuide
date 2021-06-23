b189864bd4579f217e3713a8c91ea74107b3af57
Keith Heffington <lightingcount_@hotmail.com>
2021-06-23 06:11:30 -0500

    String Skills

---
+++ Level-8/StringSkills/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringSkills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"");
+            Console.WriteLine("C:\\users\\RB\\Desktop\\MyFile.txt"); // escape sequence \
+            Console.WriteLine(@"C:\Users\RB\Desktop\MyFile.txt"); // Verbatim string literal @
+
+            int myFavoriteNumber = 26;            
+            Console.WriteLine("My favorite number is " + myFavoriteNumber + "."); // String Concatenation
+            Console.WriteLine($"My favorite number is {myFavoriteNumber}."); // String interpolation
+        }
+    }
+}
