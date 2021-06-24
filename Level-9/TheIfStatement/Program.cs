725137b9be652c093a8023dff3dc3b432c3dab19
Keith Heffington <lightingcount_@hotmail.com>
2021-06-24 09:17:02 -0500

    The If Statement

---
+++ Level-9/TheIfStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheIfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your score? ");
+            string inpput = Console.ReadLine();
+            int score = Convert.ToInt32(inpput);
+            Char letterGrade;
+            if (score == 100)
+            {
+                letterGrade = 'A';
+                Console.WriteLine("A+! Perfect score! " + letterGrade);
+            }
+            else
+            {                
+                letterGrade = 'B';
+                Console.WriteLine("Try again. " + letterGrade);
+            }
+        }
+    }
+}
