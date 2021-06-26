71c27b27c2ba88befec76fdaad89c9fff62ddfe0
Keith Heffington <lightingcount_@hotmail.com>
2021-06-26 06:46:14 -0500

    Working on if statments

--- Level-9/TheIfStatement/Program.cs
+++ Level-9/TheIfStatement/Program.cs
@@ -9,7 +9,8 @@ namespace TheIfStatement
             Console.Write("What is your score? ");
             string inpput = Console.ReadLine();
             int score = Convert.ToInt32(inpput);
-            Char letterGrade;
+            //Char letterGrade;
+            /*if else statements
             if (score == 100)
             {
                 letterGrade = 'A';
@@ -20,6 +21,75 @@ namespace TheIfStatement
                 letterGrade = 'B';
                 Console.WriteLine("Try again. " + letterGrade);
             }
+            */
+            /*else if statments
+            if (score == 100)
+            {
+                Console.WriteLine("A+! Perfect Score!");
+            } else if (score == 99)
+            {
+                Console.WriteLine("Missed it by THAT much.");  // Get Smart reference, anyone?
+            }else if (score == 42)
+            {
+                Console.WriteLine("Oh no, not again.");
+            }
+            else
+            {
+                Console.WriteLine("Try again.");
+            }
+            */
+            //Relational Operators
+            if (score >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (score >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (score >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (score >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+            Console.Write("How many points have you scored? ");
+            string input = Console.ReadLine();
+            int scored = Convert.ToInt32(input);
+
+            int pointsNeededToPass = 100;
+            bool levelComplete;
+
+            if (scored >= pointsNeededToPass)
+            {
+                levelComplete = true;
+            }
+            else
+            {
+                levelComplete = false;
+            }
+
+            if (levelComplete)
+            {
+                Console.WriteLine("You've beaten the level!");
+            }
+            else
+            {
+                Console.WriteLine("Try again!!");
+            }
+
+            /*
+             Shorter way is to run it this way
+            bool levelComplete = score >= pointsNeededToPass;
+             
+             */
+
         }
     }
 }
