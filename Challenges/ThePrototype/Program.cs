63d24a456e65ba9dcb2ca9d5a178b48e0cca2e60
Keith Heffington <lightingcount_@hotmail.com>
2021-07-01 09:28:48 -0500

    Completed The Prototype Challenge

---
+++ Challenges/ThePrototype/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ThePrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Objectives:
+                Build a program that will allow a user, the pilot, to enter a number.
+                If the number is above 100 or less than 0, keep asking.
+                Clear the screen once the program has collected a good number.
+                Ask a second user, the hunter, to guess numbers.
+                Indicate whether the user guessed too high, too low, or guessed right.
+                Loop until thye get it right, then end the program.
+             */
+
+            Console.Write("Pilot enter a number between 0 and 100: ");
+            string pilotChoice = Console.ReadLine();
+            int pilot = Convert.ToInt32(pilotChoice);
+
+            Console.Clear();
+
+            Console.WriteLine("Hunter what is your guess>?");
+
+            while (true)
+            {
+                Console.Write("What is your next guess? ");
+                int hunter = Convert.ToInt32(Console.ReadLine());
+
+                if (hunter > pilot)
+                {
+                    Console.WriteLine($"{hunter} is too high.");
+                }
+                else if (hunter < pilot)
+                {
+                    Console.WriteLine($"{hunter} is to low");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("You guessed the number!");
+        }
+    }
+}
