78fc3ad1d58995c4f0b5f29c71a161635fecf89d
Keith Heffington <lightingcount_@hotmail.com>
2021-07-06 21:43:30 -0500

    Worked with methods and passing arguments to the method

---
+++ Level-13/Methods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Create Method
+            void CountToTen()
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                    Console.WriteLine(index);
+                }
+            }
+
+            // Call Method
+            CountToTen();
+            // Call Method a second Time
+            CountToTen();
+            
+            //Passing Parameters to the Method
+            void Count(int numberToCountTo)
+            {
+                for (int index = 1; index < numberToCountTo; index++)
+                {
+                    Console.WriteLine(index);
+                }
+
+            }
+
+            //Calling the Method with a Parameter
+            Count(25);
+
+        }
+
+    }
+}
